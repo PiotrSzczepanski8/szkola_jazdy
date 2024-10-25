157478542246861c67251eac4b2ff552d75bb864
PiotrSzczepanski8 <114732037+PiotrSzczepanski8@users.noreply.github.com>
2024-10-25 07:13:57 +0200

    register process

--- client/register.php
+++ client/register.php
@@ -24,19 +24,19 @@
         <main>
             <section class="login_section">
                 <h3>Zarejestruj się</h3>
-                <form class="login_form" action="">
+                <form class="login_form" method="POST" action="../includes/register.php">
                     <label for="imie">Podaj imie</label>
-                    <input type="text">
+                    <input type="text" name="name">
                     <label for="nazwisko">Podaj nazwisko</label>
-                    <input type="text">
+                    <input type="text" name="surname">
                     <label for="phone">Podaj numer kontaktowy</label>
-                    <input type="tel" pattern="\+[0-9]{2,3} [0-9]{3} [0-9]{3} [0-9]{3}" placeholder="+48 000 000 000">
+                    <input type="tel" pattern="\+[0-9]{2,3} [0-9]{3} [0-9]{3} [0-9]{3}" placeholder="+48 000 000 000" name="p_number">
                     <label for="email">Podaj email</label>
-                    <input type="email">
+                    <input type="email" name="email">
                     <label for="login">Podaj login</label>
-                    <input type="text">
+                    <input type="text" name="login">
                     <label for="password">Podaj hasło</label>
-                    <input type="password">
+                    <input type="password" name="password">
                     <input class="login_submit" type="submit" value="Potwierdź">
                 </form>
             </section>
