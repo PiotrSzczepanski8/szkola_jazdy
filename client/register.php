780b4bcf97d14e4dbc880ec6c929bc462e907f24
PiotrSzczepanski8 <114732037+PiotrSzczepanski8@users.noreply.github.com>
2024-10-26 08:41:30 +0200

    update: register required fields

--- client/register.php
+++ client/register.php
@@ -26,17 +26,17 @@
                 <h3>Zarejestruj się</h3>
                 <form class="login_form" method="POST" action="../includes/register.php">
                     <label for="imie">Podaj imie</label>
-                    <input type="text" name="name">
+                    <input type="text" name="name" required>
                     <label for="nazwisko">Podaj nazwisko</label>
-                    <input type="text" name="surname">
+                    <input type="text" name="surname" required>
                     <label for="phone">Podaj numer kontaktowy</label>
-                    <input type="tel" pattern="\+[0-9]{2,3} [0-9]{3} [0-9]{3} [0-9]{3}" placeholder="+48 000 000 000" name="p_number">
+                    <input type="tel" pattern="\+[0-9]{2,3} [0-9]{3} [0-9]{3} [0-9]{3}" placeholder="+48 000 000 000" name="p_number" required>
                     <label for="email">Podaj email</label>
-                    <input type="email" name="email">
+                    <input type="email" name="email" required>
                     <label for="login">Podaj login</label>
-                    <input type="text" name="login">
+                    <input type="text" name="login" required>
                     <label for="password">Podaj hasło</label>
-                    <input type="password" name="password">
+                    <input type="password" name="password" required>
                     <a href="login.php" class="log_reg">mam już konto &rightarrow;</a>
                     <input class="login_submit" type="submit" value="Potwierdź">
                 </form>
