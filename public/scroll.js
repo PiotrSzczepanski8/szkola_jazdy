5ccfbe57f879a8977d0dc99b66cc7634a9a30ebb
PiotrSzczepanski8 <114732037+PiotrSzczepanski8@users.noreply.github.com>
2024-11-21 08:25:36 +0100

    fix: grabbing-scroll multiple containers

--- public/scroll.js
+++ public/scroll.js
@@ -1,33 +1,35 @@
-const scrollContainer = document.querySelector('.products_overview');
+const scrollContainers = document.querySelectorAll('.products_overview'); // Pobieramy wszystkie kontenery
 
-let isDown = false;
-let startX;
-let scrollLeft;
+scrollContainers.forEach(scrollContainer => {
+    let isDown = false; // Lokalny stan dla tego kontenera
+    let startX;
+    let scrollLeft;
 
-scrollContainer.addEventListener('mousedown', (e) => {
-    isDown = true;
-    scrollContainer.classList.add('active');
-    startX = e.pageX - scrollContainer.offsetLeft;
-    scrollLeft = scrollContainer.scrollLeft;
-    scrollContainer.style.cursor = "grabbing";
-});
+    scrollContainer.addEventListener('mousedown', (e) => {
+        isDown = true;
+        scrollContainer.classList.add('active');
+        startX = e.pageX - scrollContainer.offsetLeft;
+        scrollLeft = scrollContainer.scrollLeft;
+        scrollContainer.style.cursor = "grabbing";
+    });
 
-scrollContainer.addEventListener('mouseleave', () => {
-    isDown = false;
-    scrollContainer.style.cursor = "grab";
-    scrollContainer.classList.remove('active');
-});
+    scrollContainer.addEventListener('mouseleave', () => {
+        isDown = false;
+        scrollContainer.style.cursor = "grab";
+        scrollContainer.classList.remove('active');
+    });
 
-scrollContainer.addEventListener('mouseup', () => {
-    isDown = false;
-    scrollContainer.style.cursor = "grab";
-    scrollContainer.classList.remove('active');
-});
+    scrollContainer.addEventListener('mouseup', () => {
+        isDown = false;
+        scrollContainer.style.cursor = "grab";
+        scrollContainer.classList.remove('active');
+    });
 
-scrollContainer.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - scrollContainer.offsetLeft;
-    const walk = (x - startX);
-    scrollContainer.scrollLeft = scrollLeft - walk;
+    scrollContainer.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - scrollContainer.offsetLeft;
+        const walk = (x - startX); // Przesunięcie
+        scrollContainer.scrollLeft = scrollLeft - walk;
+    });
 });
