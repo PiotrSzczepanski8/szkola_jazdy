96b634c48c3890df90aae58f72290a38d36972ab
PiotrSzczepanski8 <114732037+PiotrSzczepanski8@users.noreply.github.com>
2025-03-03 18:45:59 +0100

    update: images paths in database

--- szkola_jazdy.sql
+++ szkola_jazdy.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Lis 26, 2024 at 08:23 AM
--- Wersja serwera: 10.4.28-MariaDB
--- Wersja PHP: 8.1.17
+-- Czas generowania: 03 Mar 2025, 18:44
+-- Wersja serwera: 10.4.24-MariaDB
+-- Wersja PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `szkola_jazdy`
+-- Baza danych: `szkola_jazdy`
 --
 CREATE DATABASE IF NOT EXISTS `szkola_jazdy` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `szkola_jazdy`;
@@ -35,28 +35,29 @@ CREATE TABLE `kurs` (
   `opis` text NOT NULL,
   `cena` decimal(6,2) NOT NULL,
   `h_praktyka` int(11) DEFAULT NULL,
-  `h_teoria` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `h_teoria` int(11) DEFAULT NULL,
+  `obrazek` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `kurs`
+-- Zrzut danych tabeli `kurs`
 --
 
-INSERT INTO `kurs` (`id_kurs`, `kategoria`, `opis`, `cena`, `h_praktyka`, `h_teoria`) VALUES
-(1, 'A', 'Dzięki temu rodzajowi prawa jazdy będziesz mógł poruszać się każdym jednośladem bez względu na pojemność czy moc silnika', 2999.99, 20, 30),
-(2, 'B', 'Prawo jazdy kategorii B pozwoli Ci prowadzić samochody osobowe, minibusy i kempingi.', 2499.99, 30, 30),
-(3, 'C', 'Kategoria C jest dla Ciebie jeśli chcesz prowadzić samochody ciężarowe', 5499.99, 25, 20),
-(4, 'D', 'Dzieki kategorii D będziesz mógł prowadzić autobusy.', 7499.99, 60, 20),
-(5, 'B+E', 'B+E to rozszerzenie kategorii B, które pozwala na prowadzenie pojazdów o większej ładowności.', 1999.99, 15, 0),
-(6, 'C+E', 'Kategoria C+E to rozszerzenie kategorii C, które pozwala Ci prowadzić zespół pojazdów bez żadnych limitów wagowych.', 6499.99, 25, 0),
-(7, 'D+E', 'Dzięki temu rozszerzeniu kategorii D, możesz prowadzić autobusy z przyczepą bez ograniczeń tonażowych.', 8999.99, 30, 0),
-(8, 'A1', 'Dzięki temu rodzajowi prawa jazdy będziesz mógł prowadzić jednoślad, jednak o mniejszej mocy i pojemności silnika niż w kategorii A.', 2499.99, 15, 30),
-(9, 'B1', 'Kategoria B1 uprawnia Cię do kierowania małym samochodem osobowym rozpędzającym się do 100km/h.', 1999.99, 20, 30),
-(10, 'C1', 'Prawo jazdy kategorii C1 uprawnia Cię do kierowania pojazdami ciężarowymi, których waga nie przekracza 7,5 tony.', 3999.99, 20, 20),
-(11, 'D1', 'Prawo jazdy ketegorii D1 pozwala Ci prowadzić autobusy o długości do 8 metrów', 5999.99, 40, 20),
-(12, 'C1+E', 'Kategoria C1+E uprawnia Cię do kierowania mniejszymi ciężarówkami z przyczepą.', 4499.99, 20, 0),
-(13, 'D1+E', 'Kategoria D1+E uprawnia Cię do kierowania zespołem pojazdów (pojazd ciągnący i przyczepa) o wadze nie przekraczającej 12t.', 5999.99, 25, 0),
-(14, 'T', 'Kategoria T uprawnia Cię do kierowania traktorem.', 2999.99, 20, 20);
+INSERT INTO `kurs` (`id_kurs`, `kategoria`, `opis`, `cena`, `h_praktyka`, `h_teoria`, `obrazek`) VALUES
+(1, 'A', 'Dzięki temu rodzajowi prawa jazdy będziesz mógł poruszać się każdym jednośladem bez względu na pojemność czy moc silnika.', '2999.99', 20, 30, 'car.webp'),
+(2, 'B', 'Prawo jazdy kategorii B pozwoli Ci prowadzić samochody osobowe, minibusy i kempingi.', '2499.99', 30, 30, 'car.webp'),
+(3, 'C', 'Kategoria C jest dla Ciebie jeśli chcesz prowadzić samochody ciężarowe', '5499.99', 25, 20, 'car.webp'),
+(4, 'D', 'Dzieki kategorii D będziesz mógł prowadzić autobusy.', '7499.99', 60, 20, 'car.webp'),
+(5, 'B+E', 'B+E to rozszerzenie kategorii B, które pozwala na prowadzenie pojazdów o większej ładowności.', '1999.99', 15, 0, 'car.webp'),
+(6, 'C+E', 'Kategoria C+E to rozszerzenie kategorii C, które pozwala Ci prowadzić zespół pojazdów bez żadnych limitów wagowych.', '6499.99', 25, 0, 'car.webp'),
+(7, 'D+E', 'Dzięki temu rozszerzeniu kategorii D, możesz prowadzić autobusy z przyczepą bez ograniczeń tonażowych.', '8999.99', 30, 0, 'car.webp'),
+(8, 'A1', 'Dzięki temu rodzajowi prawa jazdy będziesz mógł prowadzić jednoślad, jednak o mniejszej mocy i pojemności silnika niż w kategorii A.', '2499.99', 15, 30, 'car.webp'),
+(9, 'B1', 'Kategoria B1 uprawnia Cię do kierowania małym samochodem osobowym rozpędzającym się do 100km/h.', '1999.99', 20, 30, 'car.webp'),
+(10, 'C1', 'Prawo jazdy kategorii C1 uprawnia Cię do kierowania pojazdami ciężarowymi, których waga nie przekracza 7,5 tony.', '3999.99', 20, 20, 'car.webp'),
+(11, 'D1', 'Prawo jazdy ketegorii D1 pozwala Ci prowadzić autobusy o długości do 8 metrów', '5999.99', 40, 20, 'car.webp'),
+(12, 'C1+E', 'Kategoria C1+E uprawnia Cię do kierowania mniejszymi ciężarówkami z przyczepą.', '4499.99', 20, 0, 'car.webp'),
+(13, 'D1+E', 'Kategoria D1+E uprawnia Cię do kierowania zespołem pojazdów (pojazd ciągnący i przyczepa) o wadze nie przekraczającej 12t.', '5999.99', 25, 0, 'car.webp'),
+(14, 'T', 'Kategoria T uprawnia Cię do kierowania traktorem.', '2999.99', 20, 20, 'car.webp');
 
 -- --------------------------------------------------------
 
@@ -72,18 +73,15 @@ CREATE TABLE `kursant` (
   `email` varchar(100) NOT NULL,
   `haslo` varchar(100) NOT NULL,
   `login` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ;
 
 --
--- Dumping data for table `kursant`
+-- Zrzut danych tabeli `kursant`
 --
 
 INSERT INTO `kursant` (`id_kursant`, `imie`, `nazwisko`, `telefon`, `email`, `haslo`, `login`) VALUES
 (1, 'Marek', 'Rurka', '+48 517 899 345', 'marek@rurka.pl', 'Marek123', 'marekrurka'),
-(2, 'Karolina', 'Fryderyk', '+90 000 000 000', 'karolinka@chopin.pl', 'Karolina12345678!', 'aaaa'),
-(3, 'Marcin', 'Ciężczak', '+48 788 788 999', 'marcinekuwu123@p.pl', 'Marcin123#', 'marcinek'),
-(4, 'Pan', 'Piotr', '+48 696 696 696', 'panpiotr@piotr.pl', 'PanPiotr678190%$', 'panpiotr1'),
-(5, 'Krzysztof', 'Szorek', '+48 789 456 190', 'krzysiu@s.pl', 'SZorkowniscy678345', 'krzysiu');
+(2, 'Karolina', 'Fryderyk', '+90 000 000 000', 'karolinka@chopin.pl', 'Karolina12345678!', 'aaaa');
 
 -- --------------------------------------------------------
 
@@ -100,7 +98,7 @@ CREATE TABLE `lekcja` (
   `godzina` time NOT NULL,
   `id_samochod` int(11) DEFAULT NULL,
   `typ_lekcji` enum('teoria','praktyka') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -119,19 +117,20 @@ CREATE TABLE `pracownik` (
   `nr_lokalu` varchar(10) NOT NULL,
   `login` varchar(50) NOT NULL,
   `haslo` varchar(100) NOT NULL,
-  `typ_pracownika` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `typ_pracownika` varchar(20) NOT NULL,
+  `opis` text DEFAULT NULL
+) ;
 
 --
--- Dumping data for table `pracownik`
+-- Zrzut danych tabeli `pracownik`
 --
 
-INSERT INTO `pracownik` (`id_pracownik`, `imie`, `nazwisko`, `telefon`, `email`, `miasto`, `ulica`, `nr_lokalu`, `login`, `haslo`, `typ_pracownika`) VALUES
-(1, 'Karol', 'Robak', '+48 667 887 123', 'karolrobak112@rak.pl', 'Kraków', 'Wodna', '7', 'admin', 'trudneHaslo123#', 'admin'),
-(2, 'Marta', 'Koralowa', '+48 567 874 123', 'martakoralowa@koral.pl', 'Poznań', 'Rolnicza', '9', 'in_marta.k', 'kochamPieskiUwU', 'instruktor'),
-(3, 'Kamil', 'Ślimak', '+48 934 567 999', 'kamil@slimak.palindrom.pl', 'Rybnik', 'Koziołkowska', '37', 'kamilślimak', 'jestemWolny!', 'instruktor'),
-(4, 'Barbara', 'Grom', '+48 567 321 123', 'basiagrom@gmail.com', 'Warszawa', 'Długa', '19/4', 'baśkaInstruktorka', 'Pimpuś2019!', 'instruktor'),
-(5, 'Krzysztof', 'Rączka', '+48 876 871 002', 'pan@krzysztof.pl', 'Mikołów', 'Robotnicza', '7/8', 'krzy88', 'krzysiuMisiu88', 'instruktor');
+INSERT INTO `pracownik` (`id_pracownik`, `imie`, `nazwisko`, `telefon`, `email`, `miasto`, `ulica`, `nr_lokalu`, `login`, `haslo`, `typ_pracownika`, `opis`) VALUES
+(1, 'Karol', 'Robak', '+48 667 887 123', 'karolrobak112@rak.pl', 'Kraków', 'Wodna', '7', 'admin', 'trudneHaslo123#', 'admin', NULL),
+(2, 'Marta', 'Koralowa', '+48 567 874 123', 'martakoralowa@koral.pl', 'Poznań', 'Rolnicza', '9', 'in_marta.k', 'kochamPieskiUwU', 'instruktor', NULL),
+(3, 'Kamil', 'Ślimak', '+48 934 567 999', 'kamil@slimak.palindrom.pl', 'Rybnik', 'Koziołkowska', '37', 'kamilślimak', 'jestemWolny!', 'instruktor', 'Poznaj Kamila - młodego instruktora z pasją do motoryzacji i nowoczesnym podejściem do nauki jazdy. Łączy spokojną atmosferę z praktycznymi wskazówkami, pomagając kursantom budować pewność siebie za kierownicą. Z nim teoria staje się prosta, a jazdy to przyjemność, nie stres.'),
+(4, 'Barbara', 'Grom', '+48 567 321 123', 'basiagrom@gmail.com', 'Warszawa', 'Długa', '19/4', 'baśkaInstruktorka', 'Pimpuś2019!', 'instruktor', NULL),
+(5, 'Krzysztof', 'Rączka', '+48 876 871 002', 'pan@krzysztof.pl', 'Mikołów', 'Robotnicza', '7/8', 'krzy88', 'krzysiuMisiu88', 'instruktor', NULL);
 
 -- --------------------------------------------------------
 
@@ -145,10 +144,10 @@ CREATE TABLE `samochod` (
   `model` varchar(30) NOT NULL,
   `numer_rejestracyjny` varchar(8) NOT NULL,
   `stan` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `samochod`
+-- Zrzut danych tabeli `samochod`
 --
 
 INSERT INTO `samochod` (`id_samochod`, `marka`, `model`, `numer_rejestracyjny`, `stan`) VALUES
@@ -165,15 +164,16 @@ CREATE TABLE `transakcja` (
   `id_kursant` int(11) DEFAULT NULL,
   `id_kurs` int(11) DEFAULT NULL,
   `data_transakcji` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `transakcja`
+-- Zrzut danych tabeli `transakcja`
 --
 
 INSERT INTO `transakcja` (`id_transakcja`, `id_kursant`, `id_kurs`, `data_transakcji`) VALUES
 (1, 1, 3, '2024-10-31'),
-(2, 1, 4, '2024-11-07');
+(2, 1, 4, '2024-11-07'),
+(3, 1, 3, '2025-03-02');
 
 -- --------------------------------------------------------
 
@@ -186,7 +186,7 @@ CREATE TABLE `wyplata` (
   `kwota` decimal(7,2) NOT NULL,
   `id_instruktor` int(11) NOT NULL,
   `data_wyplaty` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indeksy dla zrzutów tabel
@@ -204,9 +204,6 @@ ALTER TABLE `kurs`
 --
 ALTER TABLE `kursant`
   ADD PRIMARY KEY (`id_kursant`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `login` (`login`),
-  ADD UNIQUE KEY `telefon` (`telefon`),
   ADD KEY `id_kursant` (`id_kursant`);
 
 --
@@ -224,17 +221,13 @@ ALTER TABLE `lekcja`
 -- Indeksy dla tabeli `pracownik`
 --
 ALTER TABLE `pracownik`
-  ADD PRIMARY KEY (`id_pracownik`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `login` (`login`),
-  ADD UNIQUE KEY `telefon` (`telefon`);
+  ADD PRIMARY KEY (`id_pracownik`);
 
 --
 -- Indeksy dla tabeli `samochod`
 --
 ALTER TABLE `samochod`
   ADD PRIMARY KEY (`id_samochod`),
-  ADD UNIQUE KEY `numer_rejestracyjny` (`numer_rejestracyjny`),
   ADD KEY `id_samochod` (`id_samochod`);
 
 --
@@ -253,57 +246,57 @@ ALTER TABLE `wyplata`
   ADD KEY `id_instruktor` (`id_instruktor`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT dla zrzuconych tabel
 --
 
 --
--- AUTO_INCREMENT for table `kurs`
+-- AUTO_INCREMENT dla tabeli `kurs`
 --
 ALTER TABLE `kurs`
   MODIFY `id_kurs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT for table `kursant`
+-- AUTO_INCREMENT dla tabeli `kursant`
 --
 ALTER TABLE `kursant`
-  MODIFY `id_kursant` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_kursant` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `lekcja`
+-- AUTO_INCREMENT dla tabeli `lekcja`
 --
 ALTER TABLE `lekcja`
   MODIFY `id_lekcja` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `pracownik`
+-- AUTO_INCREMENT dla tabeli `pracownik`
 --
 ALTER TABLE `pracownik`
-  MODIFY `id_pracownik` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_pracownik` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `samochod`
+-- AUTO_INCREMENT dla tabeli `samochod`
 --
 ALTER TABLE `samochod`
   MODIFY `id_samochod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `transakcja`
+-- AUTO_INCREMENT dla tabeli `transakcja`
 --
 ALTER TABLE `transakcja`
-  MODIFY `id_transakcja` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_transakcja` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `wyplata`
+-- AUTO_INCREMENT dla tabeli `wyplata`
 --
 ALTER TABLE `wyplata`
   MODIFY `id_wyplata` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Constraints for dumped tables
+-- Ograniczenia dla zrzutów tabel
 --
 
 --
--- Constraints for table `lekcja`
+-- Ograniczenia dla tabeli `lekcja`
 --
 ALTER TABLE `lekcja`
   ADD CONSTRAINT `lekcja_ibfk_1` FOREIGN KEY (`id_kursant`) REFERENCES `kursant` (`id_kursant`),
@@ -312,14 +305,14 @@ ALTER TABLE `lekcja`
   ADD CONSTRAINT `lekcja_ibfk_4` FOREIGN KEY (`id_samochod`) REFERENCES `samochod` (`id_samochod`);
 
 --
--- Constraints for table `transakcja`
+-- Ograniczenia dla tabeli `transakcja`
 --
 ALTER TABLE `transakcja`
   ADD CONSTRAINT `transakcja_ibfk_1` FOREIGN KEY (`id_kursant`) REFERENCES `kursant` (`id_kursant`),
   ADD CONSTRAINT `transakcja_ibfk_2` FOREIGN KEY (`id_kurs`) REFERENCES `kurs` (`id_kurs`);
 
 --
--- Constraints for table `wyplata`
+-- Ograniczenia dla tabeli `wyplata`
 --
 ALTER TABLE `wyplata`
   ADD CONSTRAINT `wyplata_ibfk_1` FOREIGN KEY (`id_instruktor`) REFERENCES `pracownik` (`id_pracownik`);
